12ee4bc0d444dc3366343edfde4f94aa0815535a
hsbacot <hsbacot@gmail.com>
2013-01-31 08:56:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omimages::Application.config.secret_token = '3d19b50a028a456815d985ed13d9449b2e5dfdd03cd1aaa368589c8136b5a6996ff00a595a6ca27ee3ce86870af067fe1af8dff8b3229b085cf23ac7b95f7958'
